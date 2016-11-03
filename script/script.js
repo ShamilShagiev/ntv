136c4e63256598a1382ca50cb205875fbbf0b3e6
Shamil Shagiev <that2121@mail.ru>
2016-11-03 18:40:26 +0300

    index

--- script/script.js
+++ script/script.js
@@ -1,9 +1,9 @@
-var url = "url(./Data/Images/Nature/";
+var url = "url(./data/Images/Nature/";
 var stack1 = 1;
 var stack2 = 2;
 var jpg = ".jpg"
 var end = ")"
-/*
+
 var arr = [];
 var len = 30;
 var i;
@@ -16,25 +16,32 @@ for (i = 0; i < len; i++)
 var is_pressed = 0;
 function swap(k)
 {
-	document.getElementById("image").style.backgroundImage = (url + String(arr[k]) + jpg + end);
-	document.getElementById("back").style.backgroundImage = (url + String(arr[k]) + jpg + end);
-	var swp = arr[k];
-	arr[k] = arr[k + 1];
-	arr[k + 1] = swp;
-}*/
+	document.getElementById("image").style.backgroundImage = (url + String(k) + jpg + end);
+	document.getElementById("back").style.backgroundImage = (url + String(k) + jpg + end);
+}
 
-/*function mix()
+function mix()
 {
-	for(i = len; i > 0; i--)
+	for(i = len - 1; i >= 0; i--)
 	{
 		var n = Math.floor(Math.random() * i) + 1;
 		var stck = arr[i];
 		arr[i] = arr[n];
 		arr[n] = stck;
 	}
-}*/
+}
 
-function click()
+function startbtn()
 {
-		document.getElementById("start_button").style.display = "none";
+	mix();
+	i = -1;
+	var timerId = setTimeout(function tick() {
+	i++;
+	if(i == len) return;
+	swap(arr[i]);
+	alert(i);
+	timerId = setTimeout(tick, 1000);
+	return;
+	}, 1000);
+	document.getElementById("start_button").style.display = "none";
 }
